b67bf31a7530795296163d5b610db0edce58d1a3
AndreeCrdns <127163475+AndreeCrdns@users.noreply.github.com>
2023-04-23 21:40:33 -0700

    FINAL

--- AnalizadorAFD/src/AFD.java
+++ AnalizadorAFD/src/AFD.java
@@ -2,11 +2,11 @@ import java.io.File;
 import java.util.Scanner;
 
 public class AFD {
-    private String[] alfabeto;
+    private char[] alfabeto;
     private int estados = 0;
-    private int[] finales = new int [1000];
-    private int[][] tabla = new int[1000][1000];
-    int simboloIndex = -1;
+    private int[] finales;
+    private int[][] tabla;
+    int simboloIndex = 0;
     String archivo;
 
     public AFD(String archivo) {
@@ -18,71 +18,71 @@ public class AFD {
         try {
             File arcAFD = new File(archivo);
             Scanner sc = new Scanner(arcAFD);
-    
-            alfabeto = sc.nextLine().split(",");
+
+            alfabeto = sc.nextLine().replaceAll(" ", "").toCharArray();
             for (int i = 0; i < alfabeto.length; i++) {
                 for (int j = i + 1; j < alfabeto.length; j++) {
-                    if (alfabeto[i].equals(alfabeto[j])) {
+                    if (alfabeto[i] == alfabeto[j]) {
                         System.out.println("Error, no se puede repetir los caracteres");
                     }
                 }
             }
-    
+
             estados = Integer.parseInt(sc.nextLine());
-    
-            String[] linea = sc.nextLine().split(",");
-            finales = new int[1000];
+
+            String[] linea = sc.nextLine().split(" ");
+            finales = new int[linea.length];
             for (int i = 0; i < linea.length; i++) {
                 finales[i] = Integer.parseInt(linea[i]);
                 if (finales[i] >= estados) {
                     System.out.println("Error, el estado final no se encuentra entre los estados");
                 }
             }
-    
+
             tabla = new int[estados][alfabeto.length];
             for (int i = 0; i < estados; i++) {
-                String[] fila = sc.nextLine().split(",");
-                for (int j = 0; j < alfabeto.length; j++) {
-                    tabla[i][j] = Integer.parseInt(fila[j]);
+                String lineanew = sc.nextLine();
+                if (lineanew != null) {
+                    String[] fila = lineanew.split(" ");
+                    for (int j = 0; j < alfabeto.length; j++) {
+                        tabla[i][j] = Integer.parseInt(fila[j]);
+                    }
+
+                } else {
+                    System.out.println("Linea nula");
+                    break;
                 }
             }
-    
             sc.close();
         } catch (Exception e) {
-            System.out.println("Error al leer el archivo");
+            System.out.println(e);
             return false;
         }
-    
+
         return true;
     }
-    
+
     public boolean pertenece(String cadena) {
         int estadoAct = 0;
         int estadoSig = 0;
         boolean bandera = false;
         boolean bandera2 = false;
         boolean match = false;
-        String[] cadenaArr = cadena.split(" ");
+        char[] cadenaArr = cadena.replaceAll(" ", "").toCharArray();
         for (int i = 0; i < cadenaArr.length; i++) {
             match = false;
             for (int j = 0; j < alfabeto.length; j++) {
-                if (alfabeto[j].equals(cadenaArr[i])) {
+                if (alfabeto[j] == cadenaArr[i]) {
                     match = true;
+                    estadoSig = j;
                 }
             }
             if (match == false) {
                 return false;
             }
-
-        }
-        for (int i = 0; i < estadoSig; i++) {
-            for (int j = 0; j < alfabeto.length; j++) {
-                if (alfabeto[j].equals(cadenaArr[i])) {
-                    simboloIndex = j;
-                }
-            }
             estadoSig = tabla[estadoAct][estadoSig];
             estadoAct = estadoSig;
+
         }
         for (int l = 0; l < finales.length; l++) {
             if (estadoAct == finales[l]) {
@@ -99,5 +99,10 @@ public class AFD {
         }
         return true;
     }
-
+    public int getEstados() {
+        return estados;
+    }
+    public int[][] getTabla() {
+        return tabla;
+    }
 }
--- AnalizadorAFD/src/App.java
+++ AnalizadorAFD/src/App.java
@@ -5,19 +5,19 @@ public class App {
 
         Archivos arch = new Archivos("prueba.txt");
 
-        String[]palabraspruebas; //arreglo que guarda las palabras para imprimir
+        String[] palabraspruebas; // arreglo que guarda las palabras para imprimir
         do {
-        palabraspruebas = arch.lineapalabra();
-        if(palabraspruebas[0] == "Fin del recorrido del archivo")
-        {
-            System.out.println("Ya no hay nada para analizar");
-            break;
-        }
-        for (int i = 0; i<palabraspruebas.length; i++) //empezando desde 0, hasta el final del arreglo
-        {
-            String AFD = lista.validarCad(palabraspruebas[i]); //manda llamar validarcad de lista y valida cada palabra con palabraspruebas[i]
-            System.out.println(AFD); //token contiene la cadena de caracteres
-        }
+            palabraspruebas = arch.lineapalabra();
+            if (palabraspruebas[0] == "Fin del recorrido del archivo") {
+                System.out.println("Ya no hay nada para analizar");
+                break;
+            }
+            for (int i = 0; i < palabraspruebas.length; i++) // empezando desde 0, hasta el final del arreglo
+            {
+                String AFD = lista.validarCad(palabraspruebas[i]); // manda llamar validarcad de lista y valida cada
+                                                                   // palabra con palabraspruebas[i]
+                System.out.println(AFD); // token contiene la cadena de caracteres
+            }
         } while (palabraspruebas[0] != "Fin del recorrido del archivo");
     }
-    }
+}
